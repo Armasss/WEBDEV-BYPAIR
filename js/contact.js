9c69f52deb6b08c59e8756d78e1e59600f432932
Jerome-Tegrado <tegradojerome@gmail.com>
2024-12-07 01:10:07 +0800

    files updated for contact

--- js/contact.js
+++ js/contact.js
@@ -7,12 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Check if the form is valid
         if (form.checkValidity()) {
-            // Show the notification
-            notification.style.display = "flex";
+            // Add 'show' class to display notification
+            notification.classList.remove('hide');
+            notification.classList.add('show');
 
-            // Automatically hide the notification after 3 seconds
+            // Remove 'show' class and add 'hide' class after 3 seconds
             setTimeout(() => {
-                notification.style.display = "none";
+                notification.classList.remove('show');
+                notification.classList.add('hide');
             }, 3000);
 
             // Clear the form fields after showing the notification
