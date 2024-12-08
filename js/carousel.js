550b0290188536f8cfa10936d702634fdf92aa0a
Jaytegski <tegradojerome@gmail.com>
2024-12-08 23:51:36 +0800

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
         imageInterval = setInterval(() => {
             currentImageIndex = (currentImageIndex + 1) % images.length;
             showImage(currentGameIndex, currentImageIndex);
-        }, 2100);
+        }, 2000);
     }
   
     // Start cycling words for cinematic text
